8d90cd7e1c1862d0fb5f6c3d885d4f9dfc94a4a9
HarshithMonu <42287487+HarshithMonu@users.noreply.github.com>
2024-03-26 11:49:03 +0530

    Add files via upload

---
+++ SQL Tableau.sql
@@ -0,0 +1,100 @@
+use employees_mod;
+
+SELECT 
+    YEAR(d.from_date) AS year_date, e.gender, COUNT(e.emp_no)
+FROM
+    t_employees e
+        JOIN
+    t_dept_emp d ON d.emp_no = e.emp_no
+GROUP BY year_date , e.gender
+HAVING year_date >= 1990;
+
+SELECT 
+    d.dept_name,
+    ee.gender,
+    dm.emp_no,
+    dm.from_date,
+    dm.to_date,
+    e.calendar_year,
+    CASE
+        WHEN
+            YEAR(dm.to_date) >= e.calendar_year
+                AND YEAR(dm.from_date) <= e.calendar_year
+        THEN
+            1
+        ELSE 0
+    END AS active
+FROM
+    (SELECT 
+        YEAR(hire_date) AS calendar_year
+    FROM
+        t_employees
+    GROUP BY calendar_year) e
+        CROSS JOIN
+    t_dept_manager dm
+        JOIN
+    t_departments d ON dm.dept_no = d.dept_no
+        JOIN
+    t_employees ee ON dm.emp_no = ee.emp_no
+ORDER BY dm.emp_no , calendar_year;
+
+
+
+
+select year(hire_date) as date_year from t_employees group by date_year cross join t_dept_manager dm join t_departments d on dm.dept_no = d.dept_no
+join t_employees e on dm.emp_no = e.emp_no order by dm.emp_no,date_year;
+
+
+SELECT 
+    e.gender,
+    d.dept_name,
+    ROUND(AVG(s.salary), 2) AS salary,
+    YEAR(s.from_date) AS calendar_year
+FROM
+    t_salaries s
+        JOIN
+    t_employees e ON s.emp_no = e.emp_no
+        JOIN
+    t_dept_emp de ON de.emp_no = e.emp_no
+        JOIN
+    t_departments d ON d.dept_no = de.dept_no
+GROUP BY d.dept_no , e.gender , calendar_year
+HAVING calendar_year <= 2002
+ORDER BY d.dept_no;
+
+
+
+DELIMITER $$
+CREATE PROCEDURE filter_salary (IN p_min_salary FLOAT, IN p_max_salary FLOAT)
+BEGIN
+SELECT 
+    e.gender, d.dept_name, AVG(s.salary) as avg_salary
+FROM
+    t_salaries s
+        JOIN
+    t_employees e ON s.emp_no = e.emp_no
+        JOIN
+    t_dept_emp de ON de.emp_no = e.emp_no
+        JOIN
+    t_departments d ON d.dept_no = de.dept_no
+    WHERE s.salary BETWEEN p_min_salary AND p_max_salary
+GROUP BY d.dept_no, e.gender;
+END$$
+DELIMITER ;
+
+call filter_salary(50000,90000);
+
+
+SELECT 
+    e.gender, d.dept_name, AVG(s.salary) as avg_salary
+FROM
+    t_salaries s
+        JOIN
+    t_employees e ON s.emp_no = e.emp_no
+        JOIN
+    t_dept_emp de ON de.emp_no = e.emp_no
+        JOIN
+    t_departments d ON d.dept_no = de.dept_no
+    WHERE s.salary BETWEEN 50000 AND 90000
+GROUP BY d.dept_no, e.gender;
+
